3bbae8981780bd842cde101898966f637d0ece05
soumiabouhouri <bouhourisoumia@gmail.com>
2020-12-10 13:59:10 +0100

    final
    final touch

--- src/app/my-radar-chart/my-radar-chart.component.ts
+++ src/app/my-radar-chart/my-radar-chart.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyRadarChartComponent implements OnInit {
 
+  public radarChartLabels = ['Q1', 'Q2', 'Q3', 'Q4'];
+  public radarChartData = [
+    {data: [120, 130, 180, 70], label: '2017'},
+    {data: [90, 150, 200, 45], label: '2018'}
+  ];
+  public radarChartType = 'radar';
   constructor() { }
 
   ngOnInit(): void {
